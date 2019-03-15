1c1de048c958636118614892b01547355aeb50bc
mykhail.s <mykhail.s@codeproduction.co>
2019-03-15 17:54:10 +0200

    process

---
+++ markup/dist/js/app.js
@@ -0,0 +1,19 @@
+jQuery(function() {
+	initFoundation();
+});
+
+function initFoundation() {
+	jQuery(document).foundation();
+
+	$(function() {
+		$('.search')
+		  .bind('click', function(event) {
+			$(".search-field").toggleClass("expand-search");
+	  
+			// if the search field is expanded, focus on it
+			if ($(".search-field").hasClass("expand-search")) {
+			  $(".search-field").focus();
+			}
+		  })
+	  });
+}
